60805e090d5ca6ce941a07e6ff159cd24970c0b4
Dusan Djukic <dusandj@gmail.com>
2018-04-23 10:40:42 +0200

    Check balance added. Comments added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-const loginPage = `
+/**
+ * Login page template
+ * 
+ */
+let loginPage = `
     <section class="section">
         <div class="container box">
             <h1 class="title">
@@ -28,7 +32,13 @@ const loginPage = `
         </div>
     </section>      
 `
-const loggedInPage = (key, number) => `
+/**
+ * LoggedInPage
+ * 
+ * @param {any} key 
+ * @param {any} number 
+ */
+let loggedInPage = (key, number) => `
     <section class="section">
         <div class="container box">
             <h1 class="title">
@@ -40,6 +50,13 @@ const loggedInPage = (key, number) => `
         </div>
         <div class="container box">
             <h1 class="title">
+                Balance
+            </h1>
+            <h2 id="balance" class="subtitle">
+            </h2>
+        </div>
+        <div class="container box">
+            <h1 class="title">
                 Number of transactions
             </h1>
             <p>${number}</p>
@@ -47,28 +64,69 @@ const loggedInPage = (key, number) => `
 
     </section>   
 `
-
-const renderLoggedInPage = (secretSeed) => {
-    const publicKey = keypairFromSecret(secretSeed).publicKey()
-    const noTx = server.transactions().forAccount(publicKey).call().then(page => 
-        document.getElementById('app').innerHTML = loggedInPage(publicKey, page.records.length))
+/**
+ * Check the balance of account 
+ * 
+ * @param {any} key 
+ */
+let getBalance = (key) => {
+    server.loadAccount(key).then(acc => {
+        acc.balances.forEach( balance => {
+            if (balance.asset_type === 'native') {
+                document.getElementById('balance').innerHTML = `${balance.balance} XLM`
+                console.log(`XLM : ${balance.balance}}`)
+            }
+        })
+    })
 }
 
-document.getElementById('app').innerHTML = loginPage
-const stellar = StellarSdk
-const server = new stellar.Server('https://horizon-testnet.stellar.org')
 
-const login = () => {
-    const secretSeed = document.getElementById('seed').value
+/**
+ * Renders logged in page
+ * 
+ * @param {any} secretSeed 
+ */
+let renderLoggedInPage = (secretSeed) => {
+    let publicKey = keypairFromSecret(secretSeed).publicKey()
+    let noTx = server.transactions().forAccount(publicKey).call().then(page => 
+        document.getElementById('app').innerHTML = loggedInPage(publicKey, page.records.length)).then(getBalance(publicKey))
+}
+/**
+ * Login method calls renderLoggedInPage and adds signout button
+ * 
+ */
+let login = () => {
+    let secretSeed = document.getElementById('seed').value
     renderLoggedInPage(secretSeed)
     document.getElementById('signout').innerHTML = `<button onclick="logout()" class="button">Sign out</button>`
 }
 
-const logout = () => {
+/**
+ * Logout method
+ * Show initial page
+ * 
+ */
+let logout = () => {
     document.getElementById('app').innerHTML = loginPage
     document.getElementById('signout').innerHTML = ''
 }
 
-const keypairFromSecret = (secret) => {
+/**
+ * Helper method
+ * Generate keypair from secret seed
+ * 
+ * @param {any} secret 
+ * @returns StellarSdk.Keypair
+ */
+let keypairFromSecret = (secret) => {
     return stellar.Keypair.fromSecret(secret)
 }
+
+/** 
+ * Basic setup
+ * Render login page first and set server url
+ * 
+*/
+document.getElementById('app').innerHTML = loginPage
+let stellar = StellarSdk
+let server = new stellar.Server('https://horizon-testnet.stellar.org')
