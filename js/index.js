6d13a4a06c3d4d9de9936529796b6a119bd83572
Dusan Djukic <dusandj@gmail.com>
2018-04-24 12:32:22 +0200

    Payment method added. Small fixes.

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ let loginPage = `
             <div class="columns">
                 <div class="column is-one-third">
                     <div class="field">
-                        <label class="label">Name</label>
+                        <label class="label">Your Secret Key</label>
                         <div class="control">
                             <input id="seed" class="input" type="text" placeholder="Example: SCHKBJ............ZLJ7">
                         </div>
@@ -45,7 +45,7 @@ let loggedInPage = (key, number) => `
                 Your Stellar Public Key
             </h2>
             <h2 class="subtitle">
-                ${key}
+                ${key.publicKey()}
             </h2>
             <hr>
             <div>
@@ -57,23 +57,27 @@ let loggedInPage = (key, number) => `
                         <div class="field">
                             <label class="label">To</label>
                             <div class="control">
-                                <input id="seed" class="input" type="text" placeholder="Example: SCHKBJ............ZLJ7">
+                                <input id="destination" class="input" type="text" placeholder="Example: SCHKBJ............ZLJ7">
                             </div>
                         </div>
                         <div class="field">
                             <label class="label">Amount</label>
                             <div class="control">
-                                <input id="seed" class="input" type="text" placeholder="Amount">
+                                <input id="amount" class="input" type="text" placeholder="Amount">
                             </div>
                         </div>                        
                         <div class="field">
                             <div class="control">
-                                <button class="button is-primary">Send</button>
+                                <button id="payment-button" class="button is-primary" >Send</button>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>            
+            </div>
+            <div class="content">
+                <p id="payment-error" class="error">
+                </p> 
+            </div>          
             <hr>
             <h2 class="title">
                 Transaction History
@@ -91,7 +95,9 @@ let loggedInPage = (key, number) => `
  * @param {any} key 
  */
 let getBalance = (key) => {
-    server.loadAccount(key).then(acc => {
+    server.loadAccount(key).catch(stellar.NotFoundError, () => {
+        document.getElementById('balance').innerHTML = `<p>Account not found on network. Please register the account by sending lumens to it before using the client.</p>`
+    }).then(acc => {
         acc.balances.forEach( balance => {
             if (balance.asset_type === 'native') {
                 document.getElementById('balance').innerHTML = `${balance.balance.substring(0, balance.balance.length - 5)} XLM`
@@ -108,14 +114,17 @@ let getBalance = (key) => {
  * @param {any} secretSeed 
  */
 let renderLoggedInPage = (secretSeed) => {
-    let publicKey = keypairFromSecret(secretSeed).publicKey()
-    let noTx = server.transactions().forAccount(publicKey).call().then(page => {
-        document.getElementById('app').innerHTML = loggedInPage(publicKey, page.records.length)
+    let keyPair = keypairFromSecret(secretSeed)
+    let noTx = server.transactions().forAccount(keyPair.publicKey()).call().then(page => {
+        document.getElementById('app').innerHTML = loggedInPage(keyPair, page.records.length)
         page.records.forEach(key => {
             document.getElementById('transactions').innerHTML += `<p><a href="http://testnet.stellarchain.io/tx/${key.hash}" target="_blank">${key.hash}</a></p>`
         })
+        document.getElementById('payment-button').addEventListener('click', () => {
+            sendXLM(keyPair)
+        })
     }).then(log => {
-        getBalance(publicKey)
+        getBalance(keyPair.publicKey())
     })
 }
 /**
@@ -149,6 +158,39 @@ let keypairFromSecret = (secret) => {
     return stellar.Keypair.fromSecret(secret)
 }
 
+/**
+ * Method for transferring Lumens from one account to another
+ * 
+ * @param {any} keyPair 
+ */
+let sendXLM = (keyPair) => {
+    let transaction
+    let amount = document.getElementById('amount').value
+    let destinationAddress = document.getElementById('destination').value
+    server.loadAccount(destinationAddress).catch(stellar.NotFoundError, () => {
+        // add error handling
+        document.getElementById('payment-error').innerHTML = `Destination account is not registered on network. Please enter the valid destination before sending lumens.`
+    }).then(() => {
+        return server.loadAccount(keyPair.publicKey())
+    }).then(sourceAccount => {
+        transaction = new stellar.TransactionBuilder(sourceAccount)
+            .addOperation(stellar.Operation.payment({
+                destination: destinationAddress,
+                asset: stellar.Asset.native(),
+                amount: amount
+            })).addMemo(stellar.Memo.text('Payment from light client.'))
+            .build()
+            transaction.sign(keyPair)
+            return server.submitTransaction(transaction)
+    }).then( result => {
+        document.getElementById('transactions').innerHTML += `<p><a href="http://testnet.stellarchain.io/tx/${result.hash}" target="_blank">${result.hash}</a></p>`
+        getBalance(keyPair.publicKey())
+    }).catch(err => {
+        //handle error
+        document.getElementById('payment-error').innerHTML = `Something went wrong with payment. Please try again....`
+    })
+}
+
 /** 
  * Basic setup
  * Render login page first and set server url
@@ -156,4 +198,5 @@ let keypairFromSecret = (secret) => {
 */
 document.getElementById('app').innerHTML = loginPage
 let stellar = StellarSdk
+stellar.Network.useTestNetwork()  
 let server = new stellar.Server('http://13.74.42.19:8000', {allowHttp: true})
